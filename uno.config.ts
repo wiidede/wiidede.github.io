6888e9537bf4d76797718e793e8f53b1589b000a
wiidede <wiixdede@gmail.com>
2023-08-02 09:49:41 +0800

    fix: style not effect

--- uno.config.ts
+++ uno.config.ts
@@ -14,9 +14,9 @@ export default defineConfig({
   shortcuts: [
     ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100'],
     ['tag', 'rounded bg-zinc:15 px-1 py-0.5 text-xs text-zinc-5 inline-block'],
-    ['under-base', 'no-underline relative before:(absolute bottom-0 left-0 right-0 b-b b-zinc-400/50 content-[\'\'])'],
-    ['under-anime', 'no-underline relative after:(absolute bottom-0 left-0 w-0 b-b-0 b-zinc-400 transition-all duration-500 content-[\'\'])'],
-    ['under-anime-r', 'relative after:(absolute bottom-0 right-0 w-0 b-b-0 b-zinc-400 transition-all duration-500 content-[\'\'])'],
+    ['under-base', 'no-underline relative inline-block before:(absolute bottom-0.1em left-0 right-0 b-b b-zinc-400/50 content-[\'\'])'],
+    ['under-anime', 'no-underline relative inline-block after:(absolute bottom-0.1em left-0 w-0 b-b-0 b-zinc-400 transition-all duration-500 content-[\'\'])'],
+    ['under-anime-r', 'relative inline-block after:(absolute bottom-0.1em right-0 w-0 b-b-0 b-zinc-400 transition-all duration-500 content-[\'\'])'],
     ['under-anime-hover', 'after:(w-full b-b-1)'],
     ['btn-border', 'border border-zinc/33 hover:border-zinc/67 focus:border-zinc/50 active:border-zinc/50'],
     ['flex-center', 'flex items-center justify-center'],
