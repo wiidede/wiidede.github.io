2ab921499b53cb9da16c37f58dbafd5bb1565d0e
wiidede <wiixdede@gmail.com>
2024-12-26 11:27:15 +0800

    feat: remove 备案

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import beian from '/assets/images/beian.png'
 </script>
 
 <template>
@@ -10,17 +9,5 @@ import beian from '/assets/images/beian.png'
       2020-{{ new Date().getFullYear() }} © wiidede
     </span>
     <slot />
-    <span class="mt4 flex flex-wrap justify-center gap4">
-      <a target="_blank" href="https://beian.miit.gov.cn" class="ml4 op50 hover:op80">苏ICP备2023050822号</a>
-      <a
-        href="https://beian.mps.gov.cn/#/query/webSearch?code=32050502011687"
-        rel="noreferrer" target="_blank"
-      >
-        <span class="group flex items-center gap1">
-          <img class="inline-block h-1em w-1em select-none op80 group-hover:op100" :src="beian">
-          <span class="op50 transition-opacity group-hover:op80">苏公网安备32050502011687</span>
-        </span>
-      </a>
-    </span>
   </footer>
 </template>
